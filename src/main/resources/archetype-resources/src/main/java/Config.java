541ad202d5c68c4131f0c3e23e8741a3c55fba5d
walák <feniksy@gmail.com>
2016-02-14 22:18:30 +0100

    Somehow working

---
+++ src/main/resources/archetype-resources/src/main/java/Config.java
@@ -0,0 +1,50 @@
+package ${groupId};
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource("jdbc:h2:./${artifactId}");
+        driverManagerDataSource.setDriverClassName("org.h2.Driver");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("${groupId}");
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setJpaPropertyMap(new HashMap<String, String>() {{
+            put("hibernate.hbm2ddl.auto", "update");
+            put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        }});
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean
+    JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+
+}
+
---
+++ src/main/resources/archetype-resources/src/main/java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package ${groupId}.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
